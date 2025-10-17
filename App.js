d2fa3a18af8065682cdd803b7542c8120df76b5e
KARTHI <karthi@KARTHIs-MacBook-Air.local>
2025-10-17 11:42:06 +0530

    Initial commit: React counter app

---
+++ App.js
@@ -0,0 +1,10 @@
+function App() {
+    return (
+        <div className="app">
+            <h1>React Counter App</h1>
+            <Counter />
+        </div>
+    );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
---
+++ Counter.js
@@ -0,0 +1,21 @@
+const { useState } = React;
+
+function Counter() {
+    const [count, setCount] = useState(0);
+
+    const increment = () => {
+        setCount(count + 1);
+    };
+
+    const decrement = () => {
+        setCount(count - 1);
+    };
+
+    return (
+        <div className="counter">
+            <h1>Counter: {count}</h1>
+            <button onClick={increment}>Increment</button>
+            <button onClick={decrement}>Decrement</button>
+        </div>
+    );
+}
